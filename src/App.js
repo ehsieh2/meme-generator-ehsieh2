6f324142ec1507e8ae61be9084ae2046cc893b16
Lizzy H <lizzyh@Lizzys-MBP.home>
2022-03-26 21:33:23 -0400

    making app class

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-function App() {
-  return (
-    <div className="container.is-fullhd">
-      <div class="notification is-primary">
-        <div className="title">
-        Hello React!
-        </div>
-      </div>
-    </div>
-  );
+import { Component } from "react";
+
+class App extends Component {
+  // return (
+  //   <div className="container.is-fullhd">
+  //     <div class="notification is-primary">
+  //       <div className="title">
+  //       Hello React!
+  //       </div>
+  //     </div>
+  //   </div>
+  // );
 }
 
 export default App;
